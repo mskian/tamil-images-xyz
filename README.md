5e1773ccf2bb75bb83a18e351e0e5df4a1418476
mskian <veerfun23@gmail.com>
2019-05-07 14:29:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Tamil Images
+
+![Netlify Status](https://api.netlify.com/api/v1/badges/22c7497e-8986-4e25-9628-62f7c9b19b4c/deploy-status) [![dependencies Status](https://david-dm.org/mskian/tamil-images-xyz/status.svg?style=flat-square)](https://david-dm.org/mskian/tamil-images-xyz) ![deploys by netlify](https://img.shields.io/badge/deploys%20by-netlify-00c7b7.svg?style=flat-square)
+
+Tamil Images Blog 😃
+
+## Features
+
+- Pull Contents via Ghost Content API
+- SEO
+- canonical URL
+- Schema Markup
+- Social Cards
+- Offline Support
+- PWA
+- Sitemap
+- RSS Feed
+- Google Fonts
+- Speed
+- Responsive
+- Custom 404 Error Page
+- Tag Page
+- Author Page
+- Pagination
+
+## Starter Pack
+
+Using Gastby Starter Ghost - <https://github.com/TryGhost/gatsby-starter-ghost>
+
+## LICENSE
+
+MIT
---
+++ src/utils/siteConfig.js
@@ -0,0 +1,19 @@
+module.exports = {
+    siteUrl: `https://www.tamilimages.xyz`, // Site domain. Do not include a trailing slash!
+
+    postsPerPage: 9, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+
+    siteTitleMeta: `Tamil Images`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Tamil Images - Tamil SMS, Kavithai and Quotes Images for Social Status.`, // This allows an alternative site description for meta data for pages.
+
+    shareImage: `/images/banner.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
+    shareImageWidth: 1000, // Change to the width of your default share image
+    shareImageHeight: 523, // Change to the height of your default share image
+
+    shortTitle: `Tamil Images`, // Used for App manifest e.g. Mobile Home Screen
+    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+    backgroundColor: `#2b2e3c`, // Used for Offline Manifest
+    themeColor: `#3498DB`, // Used for Offline Manifest
+
+    googleAnalyticsID: `UA-67506568-18`, // Google Analytics
+}
